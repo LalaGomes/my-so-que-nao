3e8756a1bb022374c50805b06f3287b1aac10439
Lara Lays <lara.lays.bh6@gmail.com>
2022-08-04 17:25:40 -0300

    Rota e controller funcionando

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require('express');
+
+const UsuariosRouter = require('./routers/UsuariosRouter');
+
 const app = express();
+
+//app.use('/', UsuariosRouter);
 
 app.listen(3000,()=>{console.log('Estou escutando na porta 3000')})
